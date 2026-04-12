f6d4cf3a386f14655e00bafd53f83dd366d31a48
Matchelou38 <mathis.marsande@etu.univ-grenoble-alpes.fr>
2026-04-12 18:26:46 -0400

    adding possibility to generate documentation

--- TestK3/app/build.gradle.kts
+++ TestK3/app/build.gradle.kts
@@ -2,7 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.devtools.ksp") version "2.0.21-1.0.27"}
+    id("com.google.devtools.ksp") version "2.0.21-1.0.27"
+    id("org.jetbrains.dokka") version "1.9.20"}
 
 android {
     namespace = "com.k3mobile.testk3"
