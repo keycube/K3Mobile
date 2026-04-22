ffbe056d50cae07a4c12783ca662079e395103bc
Matchelou38 <mathis.marsande@etu.univ-grenoble-alpes.fr>
2026-04-22 14:29:18 -0400

    rename of files and adding UQAC in about page

--- TestK3/app/src/main/java/com/k3mobile/testk3/ui/screens/AboutScreen.kt
+++ TestK3/app/src/main/java/com/k3mobile/testk3/ui/screens/AboutScreen.kt
@@ -1,14 +1,12 @@
 package com.k3mobile.testk3.ui.screens
 
-import android.content.Intent
-import android.net.Uri
+
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
